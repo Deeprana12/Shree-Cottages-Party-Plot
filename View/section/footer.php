c1d324a87363c8baf88c58919b0d210ca08f77dc
Deep Rana <deeprana12357@gmail.com>
2021-12-15 23:29:15 +0530

    File structure updated!!

---
+++ View/section/footer.php
@@ -0,0 +1,88 @@
+<!--Footer -->
+<footer class="footer">
+  	 <div class="containerf">
+  	 	<div class="row justify-content-center">
+  	 		<div class="footer-col col-lg-4">
+  	 			<h4>Quick links</h4>
+  	 			<ul>
+  	 				<li><a href="index.php">Home</a></li>
+  	 				<li><a href="roomservices.php">Book Now</a></li>
+  	 				<li><a href="roomservices1.php">Room services</a></li>
+             <li><a href="roomservices1.php">Plot services</a></li>
+  	 				<li><a href="image.php">Image Gallery</a></li>
+             <li><a href="video.php">Video Gallery</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		<div class="footer-col col-lg-4">
+  	 			<h4>Reach Us</h4>
+           <ul>
+              <li><i class="fa fa-map-marker fa-lg" style="color:white;size:2rem" aria-hidden="true"></i>
+                  <p style="font-size: 16px;
+                  text-transform: capitalize;
+                  color: #ffffff;
+                  text-decoration: none;
+                  font-weight: 300;
+                  color: #bbbbbb;display: inline;margin-left:0.8rem">
+                  Gujarat State Highway 26 <br>
+                  &nbsp;&nbsp;&nbsp;&nbsp;  Talala, Gujarat,
+                  India<br>
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Pincode : 362150
+                  </p>    
+              </li>
+              <li><i class="fa fa-paper-plane fa-lg" style="color:white;size:2rem" aria-hidden="true"></i>
+                <a style="display: inline;margin-left:0.8rem" href="https://www.google.com/maps/place/Shree+cottage/@21.0483,70.5231245,17z/data=!3m1!4b1!4m8!3m7!1s0x3be2ca27bd28f1d1:0xe609e025f726b7c6!5m2!4m1!1i2!8m2!3d21.0483!4d70.5253132">Locate Us</a>
+              </li>
+          </ul> 
+  	 		</div>
+  	 		<div class="footer-col col-lg-4">
+  	 			<h4>Contact Us</h4>
+  	 			<ul>
+  	 				<li><p style="font-size: 16px;
+                  text-transform: capitalize;
+                  color: #ffffff;
+                  text-decoration: none;
+                  font-weight: 300;
+                  color: #bbbbbb;display: inline;margin-left:0.8rem">Phone No. :<br>&nbsp;&nbsp;(M)+91-9825303933</p></li>
+            <li><p style="font-size: 16px;
+                  text-transform: capitalize;
+                  color: #ffffff;
+                  text-decoration: none;
+                  font-weight: 300;
+                  color: #bbbbbb;display: inline;margin-left:0.8rem"
+                  >(O) +91-265-2344499 / <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; +91-265-2338879</p></li>
+            <li><p style="font-size: 16px;
+                  text-transform: capitalize;
+                  color: #ffffff;
+                  text-decoration: none;
+                  font-weight: 300;
+                  color: #bbbbbb;display: inline;margin-left:0.8rem;
+                  @media(max-width(387px){font-size:5vw}"
+                  >Email Add.: <br>	&nbsp;&nbsp;aminpartyplot@yahoo.in</p></li>  	 				
+  	 			</ul>
+  	 		</div>
+  	 		<div class="footer-col col-lg-4">
+  	 			<h4>follow us</h4>
+  	 			<div class="social-links">
+  	 				<a href="#"><i class="fab fa-facebook-f"></i></a>
+  	 				<a href="#"><i class="fab fa-twitter"></i></a>
+  	 				<a href="#"><i class="fab fa-instagram"></i></a>
+  	 				<a href="#"><i class="fab fa-linkedin-in"></i></a>
+  	 			</div>
+  	 		</div>
+  	 	</div>
+  	 </div>     
+      <p style="margin-bottom:0.2rem;color:white;display:block;text-align:center;">
+        © Copyright 2021 Shree cottages & party plot. All rights reserved.
+      </p>
+  </footer>
+
+  
+<script src="../../assets/js/jquery.min.js"></script>
+<script src="../../assets/js/slick.min.js"></script> 
+<script src="../../assets/js/j3.js"></script> 
+<script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+<script>
+    AOS.init(
+        {duration:1000}
+    );
+</script>
